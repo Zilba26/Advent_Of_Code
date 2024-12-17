697da4ad25efd48f53046a881525bf3b7702d196
Zilba26 <basile.lebaudy@gmail.com>
2024-12-18 00:06:43 +0100

    17

--- 2024/17/1.js
+++ 2024/17/1.js
@@ -14,14 +14,15 @@ let output = "";
 for (let i = 0 ; i < program.length ; i+=2) {
     const instruction = program[i];
     const operand = program[i+1];
-    let operandCombo;
-    if (operand < 0 || operand > 6) throw new Error('Invalid operand');
+    let operandCombo = null;
+    if (operand < 0 || operand > 7) throw new Error('Invalid operand');
     if (operand <= 3) operandCombo = operand
     else operandCombo = registres[operand - 4];
+    console.log("Instruction: " + instruction + " Operand: " + operand + " OperandCombo: " + operandCombo);
 
     switch (instruction) {
         case 0: {
-            registres[0] = registres[0] / Math.pow(2, operand);
+            registres[0] = Math.floor(registres[0] / Math.pow(2, operand));
             break;
         }
         case 1: {
@@ -34,12 +35,12 @@ for (let i = 0 ; i < program.length ; i+=2) {
         }
         case 3: {
             if (registres[0] != 0) {
-
+                i = operand - 2;
             }
             break;
         }
         case 4: {
-            registres[1] = registres[1] ^ registres[1];
+            registres[1] = registres[1] ^ registres[2];
             break;
         }
         case 5: {
@@ -48,11 +49,11 @@ for (let i = 0 ; i < program.length ; i+=2) {
             break;
         }
         case 6: {
-            registres[1] = registres[0] / Math.pow(2, operand);
+            registres[1] = Math.floor(registres[0] / Math.pow(2, operand));
             break;
         }
         case 7: {
-            registres[2] = registres[0] / Math.pow(2, operand);
+            registres[2] = Math.floor(registres[0] / Math.pow(2, operand));
             break;
         }
         default: throw new Error('Invalid instruction');
@@ -61,4 +62,6 @@ for (let i = 0 ; i < program.length ; i+=2) {
     console.log(registres.map((elt, index) => "Registre " + (index+1) + ": " + elt).join('\n'), '\n');
 }
 
-console.log(output);
+console.log(output.split(',').join(''));
+
+// ! 042156051
